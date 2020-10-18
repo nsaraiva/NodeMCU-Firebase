34a324d997d6a83ce6c2daa7f93a64264b9664ff
Nelson Luiz Silva Saraiva <provider.nlsaraiva@terceiros.mag.com.br>
2020-10-18 17:13:00 -0300

    First commit

---
+++ NodeMCU-DHT11/NodeMCU-DHT11.ino
@@ -0,0 +1,118 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <FirebaseArduino.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#include "credentials.h"
+
+
+#define DHTPIN    D2
+#define DHTTYPE   DHT11
+#define BAUDRATE  115200
+
+
+unsigned long prevMillis = 0; //Previous milliseconds
+const long interval = 2000;
+
+float humidity;     // percentage
+float temperature;  //Celsius
+
+DHT dht(DHTPIN, DHTTYPE);
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+void setup() {
+  Serial.begin(BAUDRATE);
+  connectToWiFi(SSID, PASSWORD);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  timeClient.begin();
+  dht.begin();
+}
+
+void loop() {
+  // Milliseconds passed since the program started
+  unsigned long currMillis = millis();
+
+  // Sending data according to the interval
+  if (currMillis - prevMillis >= interval) {
+    prevMillis = currMillis;     
+    timeClient.update();
+    readSensors();
+    displaySensors(humidity, temperature);
+    //sendDataToFirebase(humidity, temperature);
+  }
+}
+
+void connectToWiFi(char const *ssid, char const *password) {
+  // Setting the ESP8266 to be a WiFi-client
+  WiFi.mode(WIFI_STA);  
+  
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+
+  // connecting to WiFi
+  WiFi.disconnect();
+  WiFi.begin(ssid, password);
+  
+  // Keep Loading while the wifi is not connected
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.print("WiFi connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("");
+}
+
+void readSensors() {
+  // Reading Humidity value
+  humidity = dht.readHumidity();
+  
+  // Reading temperature value
+  temperature = dht.readTemperature();
+  
+  // Check if failed and exit.
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println(F("Failed to read!"));
+    return;
+  }
+}
+
+void displaySensors(float humidity, float temperature) {
+  Serial.println(timeClient.getFormattedTime());
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println("°C");
+}
+
+void sendDataToFirebase(float humidity, float temperature) {
+  String humValue = Firebase.pushFloat("dht11/humidity/Value", humidity);
+  if (Firebase.failed()) {
+    Serial.print("\nError pushing /dht11/humidity/Value failed: " + humValue);
+    Serial.println(Firebase.error());
+    return;
+  }
+
+  String tempValue = Firebase.pushFloat("dht11/temperature/Value", temperature);
+  if (Firebase.failed()) {
+    Serial.print("\nError pushing /dht11/temperature/Value failed:" + tempValue);
+    Serial.println(Firebase.error());
+    return;
+  }
+}
---
+++ NodeMCU-DHT11/credentials.h
@@ -0,0 +1,5 @@
+#define SSID "Trojan"
+#define PASSWORD "einstein1879"
+
+#define FIREBASE_HOST "sensorando.firebaseio.com"
+#define FIREBASE_AUTH "UeHpHJG2QEwNhgUi4hZS29HNUGfmmcEiNarXgNsc"
